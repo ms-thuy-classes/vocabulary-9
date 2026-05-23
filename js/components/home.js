7839b40ae7a95f77a30024c96649a4ab005a6a78
ms-thuy-classes <sokieua@gmail.com>
2026-05-23 20:04:26 +0700

    Add LESSONS_DATA object with lesson details

---
+++ js/components/home.js
@@ -0,0 +1,12 @@
+const LESSONS_DATA = {
+  lesson1: {
+    title: "Unit 1: Family Members",
+    icon: "🏡", // Thêm icon đại diện cho bài học ở đây
+    vocabList: [ /* từ vựng */ ]
+  },
+  lesson2: {
+    title: "Unit 2: School Things",
+    icon: "🎒",
+    vocabList: [ /* từ vựng */ ]
+  }
+};
