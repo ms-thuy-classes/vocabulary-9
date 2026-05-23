2f3bc737bace5ce2f4b558aed84d78140a452268
ms-thuy-classes <sokieua@gmail.com>
2026-05-23 20:05:33 +0700

    Update and rename home.js to data.js

---
+++ js/components/data.js
@@ -0,0 +1,16 @@
+const LESSONS_DATA = {
+  lesson1: {
+    title: "Unit 1: Family Members",
+    vocabList: [
+      { word: "Father", meaning: "Bố", emoji: "👨" },
+      { word: "Mother", meaning: "Mẹ", emoji: "👩" }
+    ]
+  },
+  lesson2: {
+    title: "Unit 2: School Things",
+    vocabList: [
+      { word: "Book", meaning: "Quyển sách", emoji: "📘" },
+      { word: "Pencil", meaning: "Bút chì", emoji: "✏️" }
+    ]
+  }
+};
