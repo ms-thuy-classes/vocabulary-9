d2b46012de939459dfbeedf179a1da776543c092
ms-thuy-classes <sokieua@gmail.com>
2026-05-23 20:07:42 +0700

    Update data.js

--- js/components/data.js
+++ js/components/data.js
@@ -1,6 +1,7 @@
 const LESSONS_DATA = {
   lesson1: {
     title: "Unit 1: Family Members",
+    icon: "🏡",
     vocabList: [
       { word: "Father", meaning: "Bố", emoji: "👨" },
       { word: "Mother", meaning: "Mẹ", emoji: "👩" }
@@ -8,6 +9,7 @@ const LESSONS_DATA = {
   },
   lesson2: {
     title: "Unit 2: School Things",
+    icon: "🎒",
     vocabList: [
       { word: "Book", meaning: "Quyển sách", emoji: "📘" },
       { word: "Pencil", meaning: "Bút chì", emoji: "✏️" }
